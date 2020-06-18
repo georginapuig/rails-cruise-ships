05ecf6a0a6e0dda9056315464b820782b1a4f609
georgina puig <georginaxcodes@gmail.com>
2020-06-18 15:19:04 -0500

    added markers

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,22 @@
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+  }
+};
+
+export { initMapbox };
