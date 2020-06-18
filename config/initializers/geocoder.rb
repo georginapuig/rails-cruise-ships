7a2b42b737d421fc8e3322077cca9dff7de9d805
georgina puig <georginaxcodes@gmail.com>
2020-06-18 14:15:51 -0500

    ships geocoded controller

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "pk.eyJ1IjoiZ2VvcmdpbmFwIiwiYSI6ImNrYXNsZ2ppMTAyZ2Iyc2xreTBzZTdyYzMifQ.aO45UTw-akvqVaHSlZ2etA",               # API key for geocoding service
+  api_key: ENV['MAPBOX_API_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
