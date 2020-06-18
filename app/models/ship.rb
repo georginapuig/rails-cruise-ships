1c3b6316e8e5d80fa59e87386ad49e7c16320166
georgina puig <georginaxcodes@gmail.com>
2020-06-18 12:55:22 -0500

    ship model geocoder

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,5 +2,9 @@ class Ship < ApplicationRecord
   belongs_to :user
   validates :name, :location, :capacity, :description, :destination, presence: true
   validates :name, uniqueness: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   has_one_attached :photo
 end
