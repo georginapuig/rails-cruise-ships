dbce1ef3174aa396415b2c5f6ecf22c8bd085c62
Carlos Palomino <palominocp@outlook.com>
2020-06-18 16:39:01 -0600

    Search feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @ships = Ship.limit(6)
+    if params[:location].present?
+      @ships = Ship.search_by_location_and_destination(params[:location])
+    else
+      @ships = Ship.limit(6)
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,10 @@
 class Ship < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_location_and_destination,
+    against: [ :location, :destination ],
+    using: {
+      tsearch: { prefix: true }
+    }
   belongs_to :user
   validates :name, :location, :capacity, :description, :destination, presence: true
   validates :name, uniqueness: true
