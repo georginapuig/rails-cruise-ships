d3dbf6e8b4a8ff759f250df9755b52b12804c9c2
georgina puig <georginaxcodes@gmail.com>
2020-06-19 01:34:40 -0500

    custom markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,9 +18,20 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
+        .addTo(map)
+        .setPopup(popup); // add this
     });
     fitMapToMarkers(map, markers);
   }
